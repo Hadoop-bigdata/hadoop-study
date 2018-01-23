d23d67a021fc00a774b5189d19e1b574183f1008
Weiyan Wu <32626769+weiyanwuda@users.noreply.github.com>
2018-01-22 23:12:36 -0500

    Update start_nosudo.sh

--- start_nosudo.sh
+++ start_nosudo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker network create --driver=bridge hadoop
+docker network create --driver=bridge hadoop
 
   
 # the default node number is 3
@@ -8,9 +8,9 @@ N=${1:-3}
 
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
+docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
-sudo docker run -itd \
+docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
@@ -24,9 +24,9 @@ sudo docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
-        sudo docker rm -f hadoop-slave$i &> /dev/null
+        docker rm -f hadoop-slave$i &> /dev/null
         echo "start hadoop-slave$i container..."
-        sudo docker run -itd \
+        docker run -itd \
                         --net=hadoop \
                         --name hadoop-slave$i \
                         --hostname hadoop-slave$i \
@@ -35,4 +35,4 @@ do
 done
 
 # get into hadoop master container
-sudo docker exec -it hadoop-master bash
+docker exec -it hadoop-master bash
