14c3c23e5cc3a89a85752e9f9994dff717a13adf
Weiyan Wu <32626769+weiyanwuda@users.noreply.github.com>
2018-01-22 23:33:38 -0500

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-sudo docker network create --driver=bridge hadoop
-
-  
 # the default node number is 3
 N=${1:-3}
 
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
+docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
-sudo docker run -itd \
+          docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
-                -p 8888:8888 \
                 --name hadoop-master \
                 --hostname hadoop-master \
                 hadoopstudy/hadoop_bigdata &> /dev/null
@@ -24,15 +20,15 @@ sudo docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
-        sudo docker rm -f hadoop-slave$i &> /dev/null
-        echo "start hadoop-slave$i container..."
-        sudo docker run -itd \
-                        --net=hadoop \
-                        --name hadoop-slave$i \
-                        --hostname hadoop-slave$i \
-                        hadoopstudy/hadoop_bigdata &> /dev/null
-        i=$(( $i + 1 ))
-done
+	docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+                  docker run -itd \
+	                --net=hadoop \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                hadoopstudy/hadoop_bigdata &> /dev/null
+	i=$(( $i + 1 ))
+done 
 
 # get into hadoop master container
-sudo docker exec -it hadoop-master bash
+docker exec -it hadoop-master bash
